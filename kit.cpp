66c1a66a047d7f5b98ef126fa91dbb051ad3299e
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-10-11 11:29:21 +0600

    Update kit.cpp

--- kit.cpp
+++ kit.cpp
@@ -55,6 +55,44 @@
 #define sortD(a)  sort(a,a+n,greater<int>())
 #define sortA(a)  sort(a,a+n)
 
+#define ll                  long long
+#define scl(n)              scanf("%lld",&n)
+#define scc(c)	            scanf("%c",&c)
+#define fr(i,n)             for (ll i=0;i<n;i++)
+#define fr1(i,n)            for(ll i=1;i<=n;i++)
+#define pfl(x)              printf("%lld\n",x)
+#define pb                  push_back
+#define debug               cout<<"I am here"<<endl;
+#define pno                 cout<<"NO"<<endl
+#define pys                 cout<<"YES"<<endl
+
+#define l(s)                s.size()
+#define asort(a)            sort(a,a+n)
+#define all(x) 	            (x).begin(), (x).end()
+#define dsort(a)            sort(a,a+n,greater<int>())
+#define vasort(v)           sort(v.begin(), v.end());
+#define vdsort(v)           sort(v.begin(), v.end(),greater<int>());
+#define uniquee(x)          x.erase(unique(x.begin(), x.end()),x.end())
+#define pn                  printf("\n")
+#define el                  cout<<endl
+#define md                  1000000007
+#define inf                 1e18
+#define ps                  cout<<" ";
+#define Pi                  acos(-1.0)
+#define pr                  pair<ll, ll>
+#define ff                  first
+#define ss                  second
+#define mem(a,i)            memset(a, i, sizeof(a))
+#define tcas(i,t)           for(ll i=1;i<=t;i++)
+#define pcas(i)             cout<<"Case "<<i<<": "<<endl;
+#define fast 	ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+
+
+#define N 200006
+ll coins[N];
+map<ll, ll>mp;
+ll pow2[N];
+
 ll func(ll n)
 {
     ll sum=0;
